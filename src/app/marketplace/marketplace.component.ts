4357b19a0ffa29230512bc3b6bef2c59ca690598
Kyle Tenzeldam and Ross Fletcher <ross@>
2018-02-26 11:51:28 -0800

    have working firebase database

--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -1,37 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { Album } from '../album.model';
 import { Router } from '@angular/router';
+import { AlbumService } from '../album.service';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-marketplace',
   templateUrl: './marketplace.component.html',
-  styleUrls: ['./marketplace.component.css']
+  styleUrls: ['./marketplace.component.css'],
+  providers: [AlbumService]
 })
 export class MarketplaceComponent implements OnInit {
+  albums: FirebaseListObservable<any[]>;
 
-  constructor(private router: Router){}
+  constructor(private router: Router, private albumService: AlbumService) {}
 
   ngOnInit() {
+    this.albums = this.albumService.getAlbums();
   }
 
-  albums: Album[] = [
-     new Album("Pulse", "Pink Floyd",
-         "A live  album by the English progressive rock band originally released in 1995, on the label EMI in the United Kingdom.", "Rock", 1),
-     new Album("Funhouse", "The Stooges",
-         "The second  album from the American rock band, released in 1970 by Elektra Records.", "Rock", 2),
-     new Album("Twilight of the Thunder God", "Amon Amarth",
-         "Seventh album by the Swedish band, released in 2008, based on Thor's battle with the serpent Jörmungandr.", "Metal", 3),
-     new Album("Dilate", "Ani DiFranco",
-         "Her highest-selling and most acclaimed album, released in 1996.", "Multi-instrument", 4),
-     new Album("Chopin - Complete Nocturnes", "Brigitte Engerer",
-         "Released in 2010, this is Engerer's own rendition of the classical composer Chopin.", "Piano", 5),
-     new Album("Axis Bold As Love", "The Jimi Hendrix Experience",
-         "Second studio album by the English-American band, released in 1967.", "Rock", 6)
-   ];
-
    goToDetailPage(clickedAlbum: Album) {
-     this.router.navigate(['albums', clickedAlbum.id]);
-   };
+    //  this.router.navigate(['albums', clickedAlbum.id]);
+   }
 
 
 }
